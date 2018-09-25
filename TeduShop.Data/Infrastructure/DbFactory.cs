f8cf73d8fbd3f7b0a5269448e06505e1205d8c80
goverysea <disgby3007@gmail.com>
2018-09-25 22:00:02 +0700

    Add to data

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,21 @@
+﻿namespace TeduShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+
+        TeduShopDbContext dbContext;
+
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if(dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+    }
+}
