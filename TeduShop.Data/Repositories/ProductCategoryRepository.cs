5e8ae9d8f57953e4a12f47003d244e38b76cf08d
goverysea <disgby3007@gmail.com>
2018-09-26 00:07:28 +0700

    migration to database

---
+++ TeduShop.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+
+    public interface IProductCategoryRepository
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
+    {
+
+        public ProductCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+
+        IEnumerable<ProductCategory> IProductCategoryRepository.GetByAlias(string alias)
+        {
+            return this.DbContext.ProductCategorys.Where(x => x.Alias == alias);
+        }
+    }
+}
